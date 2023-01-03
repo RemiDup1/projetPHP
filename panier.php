e7296494e694ecd2e53291de667a860bc1c7d88f
eetchever004 <etcheverry.eliott10@gmail.com>
2023-01-03 16:58:15 +0100

    panier

--- panier.php
+++ panier.php
@@ -1,36 +1,20 @@
 <!DOCTYPE html>
-
-<?php 
-	include 'bd.php';
-?>
-
-<html lang="fr">
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width-device-width, initial-scale=0.1">
-
-	<link rel="stylesheet" href="style.css">
-	<title>Projet - php</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Panier</title>
 </head>
 <body>
-	<header>
-		<h1>Achat et revente de CD</h1>
-		<nav>
-            <h2>/*Le Nom du site Web*/</h2>
-			<ul>
-				<li><a href="panier.php">Mon panier</i><a></li>
-                <div></div>              
-                
-			</ul>
-		</nav>
-        <hr>
-	</header>
-	<main>
-        <?php
-        while($donnees=mysqli_fetch_assoc($result)){
+    <h1>Panier</h1>
+    <?php
+
+    include 'bd.php';
+    while($donnees=mysqli_fetch_assoc($result)){
+        if ($donnees["panier"] == 1) {
             echo('<section class="articles">');
-            echo('<a href="./selectionner.php">');
             $id=$donnees["id"];
             $titre=$donnees["titre"];
             $auteur=$donnees["auteur"];
@@ -44,14 +28,10 @@
             echo ('<div class = ele1>'.$titre.'</div>');
             echo ('<div class = ele2>'.$auteur.'</div>');
             echo ('<div class = ele3>'.$prix.' €</div>') ;
-            echo('</button>');
-            echo ('</a>');
             echo ('</section>');
         }
-        
-        ?>
-	</main>
-	<footer>
-	</footer>
+    }
+
+    ?>
 </body>
 </html>
